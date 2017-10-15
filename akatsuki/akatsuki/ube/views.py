09454d6bd0cf11b30cdd3d6fc8f35d39ddf1ef4b
Marlon <martianworm17@gmail.com>
2017-10-15 10:29:43 +0800

    Create hardcoded view

--- akatsuki/akatsuki/ube/views.py
+++ akatsuki/akatsuki/ube/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def hello(request):
+    text = """<h1>Welcome to my Ube app!</h1>"""
+    return HttpResponse(text)
